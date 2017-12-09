05f0093895c498085c3be3078da02bd4290a30d9
Arseny Batyrov <comolder@gmail.com>
2017-12-09 21:52:45 +0300

    commit

--- features/area_screen.feature
+++ features/area_screen.feature
@@ -1,11 +1,18 @@
 Feature: User is able to convert area units
 
-  Scenario:
+  Scenario: User is able to dismiss help
     Given I land on Help popup
     When I click on Got it button
     Then I land on Area screen
 
-
+  @wip
+  Scenario: User is able to swap values
+    Given I click on Got it button
+    Given I see "Sq Kilometre" in From header
+    Given I see "Sq Metre" in To header
+    When I click on Swap button
+    And I see "Sq Metre" in From header
+    And I see "Sq Kilometre" in To header
 
 
 
--- features/step_definitions/area_screen_steps.rb
+++ features/step_definitions/area_screen_steps.rb
@@ -9,3 +9,19 @@ end
 Given(/^I land on Help popup$/) do
   text("Help")
 end
+
+And(/^I see "([^"]*)" in From header$/) do |value|
+  actual_value = find_element(id: "header_text_unit_from").text
+  puts("Expected value is " + value)
+  puts("Actual value is " + actual_value)
+end
+
+And(/^I see "([^"]*)" in To header$/) do |value|
+  actual_value = find_element(id: "header_text_unit_to").text
+  puts("Expected value is " + value)
+  puts("Actual value is " + actual_value)
+end
+
+When(/^I click on Swap button$/) do
+  find_element(id: "fab").click
+end
