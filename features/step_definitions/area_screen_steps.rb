9cd89b6fe505db2f5393ff08281e9cf085d33f68
comolder <comolder@gmail.com>
2017-12-10 23:27:46 +0300

    Fix

--- features/step_definitions/area_screen_steps.rb
+++ features/step_definitions/area_screen_steps.rb
@@ -9,3 +9,27 @@ end
 Given(/^I land on Help popup$/) do
   text("Help")
 end
+
+When(/^I click on Swap button$/) do
+  puts("User clicks Swap button")
+end
+
+Then(/^I see "([^"]*)" in From header$/) do |value|
+  puts("From header values is " + value)
+end
+
+And(/^I see "([^"]*)" in To header$/) do |value|
+  puts("To header values is #{value}")
+end
+
+And(/^I click on Clear button$/) do
+  puts("User clicks on Clear button")
+end
+
+When(/^I enter "([^"]*)" to From field$/) do |value|
+  puts("User entered value #{value}")
+end
+
+Then(/^I get "([^"]*)" in To field$/) do |value|
+  puts("User sees #{value} in result field")
+end
